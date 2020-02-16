b02f08fd34c408a43a782996b0d6987ff5f97f75
idogoorevitch <idogoorevitch@gmail.com>
2020-02-16 12:00:01 +0200

    changes in eurovision.cpp

--- eurovision.cpp
+++ eurovision.cpp
@@ -1,4 +1,19 @@
-//
-// Created by jack on 16/02/2020.
-//
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+typedef char* string;
+
+class participant{
+    const string country;
+    string song;
+    string performer;
+    int song_length;
+    bool is_registered;
+public:
+    void update()
+};
 
